5689c82e752c6288093e5cc5e9dce4002cf66cef
Ryan Morr <rm.morr@gmail.com>
2016-09-03 17:30:20 -0400

    Export individual module and base namespaces

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Import modules
+ */
+import * as lang from './lang';
+import * as array from './array';
+
+/**
+ * Export module namespaces individually
+ */
+export lang;
+export array;
+
+/**
+ * Declare and export `base` namespace
+ */
+const base = {lang, array};
+export default base;
